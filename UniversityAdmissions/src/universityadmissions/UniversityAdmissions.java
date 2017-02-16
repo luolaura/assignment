0e573f9e87aca54f2981230617cfc8835e7d5261
luolaura <unicornsky11@gmail.com>
2017-02-15 20:39:05 -0500

    Add files via upload

--- UniversityAdmissions/src/universityadmissions/UniversityAdmissions.java
+++ UniversityAdmissions/src/universityadmissions/UniversityAdmissions.java
@@ -3,17 +3,45 @@
  * and open the template in the editor.
  */
 package universityadmissions;
-
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 /**
  *
  * @author LHY
  */
 public class UniversityAdmissions {
-
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        FileReader AdmissionFile = new FileReader("Admission.txt");
+        Scanner scan =new Scanner(AdmissionFile);
+        PrintWriter myOutputFile = new PrintWriter("Accpeted File");
+        myOutputFile.println("Name" + "\t" + "Result");
+        while(scan.hasNext()){
+            String name = scan.next();
+            
+            String portfolio = scan.next();
+            
+            int average = scan.nextInt();
+            
+            int clubs = scan.nextInt();
+            
+            String artContest = scan.next();
+            
+            int goodDrawing = scan.nextInt();
+            
+            String photoshop =scan.next();
+            
+            int artMark = scan.nextInt();
+          
+        }
+        if (portfolio.equals("yes")){
+            
+        }
+        myOutputFile.close();
     }
 }
