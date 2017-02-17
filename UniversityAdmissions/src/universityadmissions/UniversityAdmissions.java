a17f7e7b38493698c38715da91add39ecf94731a
luolaura <unicornsky11@gmail.com>
2017-02-17 11:04:53 -0500

    Add files via upload

--- UniversityAdmissions/src/universityadmissions/UniversityAdmissions.java
+++ UniversityAdmissions/src/universityadmissions/UniversityAdmissions.java
@@ -1,6 +1,5 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Art University Admissions 
  */
 package universityadmissions;
 import java.io.FileReader;
@@ -9,7 +8,7 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 /**
  *
- * @author LHY
+ * @author Laura
  */
 public class UniversityAdmissions {
     
@@ -19,10 +18,12 @@ public class UniversityAdmissions {
     public static void main(String[] args) throws IOException {
         FileReader AdmissionFile = new FileReader("Admission.txt");
         Scanner scan =new Scanner(AdmissionFile);
-        PrintWriter myOutputFile = new PrintWriter("Accpeted File");
-        myOutputFile.println("Name" + "\t" + "Result");
+        PrintWriter myOutputFile = new PrintWriter("AccpetedOrRejected File");
+        myOutputFile.println("Name" + "\t" + "\t" + "\t" + "Result");
         while(scan.hasNext()){
-            String name = scan.next();
+            String FirstName = scan.next();
+            
+            String LastName = scan.next();
             
             String portfolio = scan.next();
             
@@ -37,16 +38,28 @@ public class UniversityAdmissions {
             String photoshop =scan.next();
             
             int artMark = scan.nextInt();
+            String name = FirstName + " " + LastName;
             
-            if (portfolio.equals("yes") && (goodDrawing >= 2 || artMark >= 85) && clubs >= 3){
+            if (portfolio.equals("yes") && (goodDrawing >= 2 || artMark >= 85) 
+                    && clubs >= 3){
                 String Accpted ="Accpted";
-                myOutputFile.println(name + "\t" + Accpted);
-            }else if (photoshop.equals("yes") && artContest.equals("yes") || average >= 90){
+                myOutputFile.println(name + "\t" + "\t" + Accpted);
+            }else if (photoshop.equals("yes") && artContest.equals("yes") 
+                    || average >= 90){
                 String Accpted ="Accpted";
-                myOutputFile.println(name + "\t" + Accpted);
+                myOutputFile.println(name + "\t" + "\t" + Accpted);
+            }else if (average >= 80 && artMark >= 90 && clubs >=4){
+                String Accpted ="Accpted";
+                myOutputFile.println(name + "\t" + "\t" + Accpted);
+            }else if (artContest.equals("yes")||goodDrawing >= 4){
+                String Accpted ="Accpted";
+                myOutputFile.println(name + "\t" + "\t" + Accpted);
+            }else{
+                String Rejected = "Rejected";
+                myOutputFile.println(name + "\t" + "\t" + Rejected);
             }
-        }
+        }//end of while loop
         
         myOutputFile.close();
-    }
-}
+    } //end of main
+}//end of class
